4e174d4c5b3dd5e5da412cea08853abb579f3d28
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2017-10-09 00:40:11 +0300

    #16 done

--- translator-web-meta/translator-web-app/src/main/java/com/github/bogdanovmn/translator/web/app/service/ToRememberService.java
+++ translator-web-meta/translator-web-app/src/main/java/com/github/bogdanovmn/translator/web/app/service/ToRememberService.java
@@ -11,7 +11,6 @@ import com.github.bogdanovmn.translator.web.orm.repository.domain.WordRepository
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
--- translator-web-meta/translator-web-orm/src/main/java/com/github/bogdanovmn/translator/web/orm/repository/app/UserHoldOverWordRepository.java
+++ translator-web-meta/translator-web-orm/src/main/java/com/github/bogdanovmn/translator/web/orm/repository/app/UserHoldOverWordRepository.java
@@ -2,7 +2,6 @@ package com.github.bogdanovmn.translator.web.orm.repository.app;
 
 import com.github.bogdanovmn.translator.web.orm.entity.app.User;
 import com.github.bogdanovmn.translator.web.orm.entity.app.UserHoldOverWord;
-import com.github.bogdanovmn.translator.web.orm.entity.app.UserRememberedWord;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
