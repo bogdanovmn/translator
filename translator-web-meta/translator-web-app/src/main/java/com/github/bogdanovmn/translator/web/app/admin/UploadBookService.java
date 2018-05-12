4b0990860514758f9b217b4006baf8653087ff31
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2018-05-12 11:27:35 +0300

    upload file transaction fix

--- translator-web-meta/translator-web-app/src/main/java/com/github/bogdanovmn/translator/web/app/admin/UploadBookService.java
+++ translator-web-meta/translator-web-app/src/main/java/com/github/bogdanovmn/translator/web/app/admin/UploadBookService.java
@@ -4,6 +4,8 @@ import com.github.bogdanovmn.translator.core.EnglishText;
 import com.github.bogdanovmn.translator.core.TranslateServiceUploadDuplicateException;
 import com.github.bogdanovmn.translator.parser.pdf.PdfInputStreamContent;
 import com.github.bogdanovmn.translator.web.orm.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 
 @Service
 public class UploadBookService {
+	private static final Logger LOG = LoggerFactory.getLogger(UploadBookService.class);
+
 	@Autowired
 	private WordRepository wordRepository;
 	@Autowired
@@ -25,8 +28,8 @@ public class UploadBookService {
 	@Autowired
 	private EntityFactory entityFactory;
 
-	@Transactional
-	public Source upload(MultipartFile file)
+	@Transactional(rollbackFor = Exception.class)
+	public synchronized Source upload(MultipartFile file)
 		throws IOException, TranslateServiceUploadDuplicateException
 	{
 		String fileMd5 = DigestUtils.md5DigestAsHex(file.getBytes());
@@ -41,6 +44,7 @@ public class UploadBookService {
 			);
 		}
 
+		LOG.info("Parse PDF file");
 		PdfInputStreamContent content = new PdfInputStreamContent(
 			file.getInputStream()
 		);
@@ -48,6 +52,7 @@ public class UploadBookService {
 		EnglishText text = new EnglishText(
 			content.getText()
 		);
+		LOG.info("Prepare words");
 
 		Collection<String> words = text.normalizedWords();
 
@@ -82,9 +87,5 @@ public class UploadBookService {
 		}
 
 		return source;
-	}
-
-	public List<Object> getHistory() {
-		return null;
 	}
 }
